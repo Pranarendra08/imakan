debeb4ab5ee93a1235e3649159b6a4ed4aad62b7
Pranarendra08 <pranarendra@gmail.com>
2022-02-02 16:50:20 +0700

    Initial commit

---
+++ app/src/main/java/com/rendra/imakan/OnboardingActivity1.kt
@@ -0,0 +1,11 @@
+package com.rendra.imakan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding1)
+    }
+}
---
+++ app/src/main/java/com/rendra/imakan/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.rendra.imakan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this, OnboardingActivity1::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
