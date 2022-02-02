48fba4e790fea4e5fe0cec07488e824915bacce1
Pranarendra08 <pranarendra@gmail.com>
2022-02-02 17:16:44 +0700

    update splash screen & onboarding screen 1

--- app/src/main/java/com/rendra/imakan/OnboardingActivity1.kt
+++ app/src/main/java/com/rendra/imakan/OnboardingActivity1.kt
@@ -1,11 +1,22 @@
 package com.rendra.imakan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rendra.imakan.signin.SignInActivity
+import com.rendra.imakan.utils.Preferences
+import kotlinx.android.synthetic.main.activity_onboarding1.*
 
 class OnboardingActivity1 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding1)
+
+        btn_lanjut.setOnClickListener {
+            finishAffinity()
+            startActivity(Intent(this, OnboardingActivity2::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/rendra/imakan/utils/Preferences.kt
@@ -0,0 +1,22 @@
+package com.rendra.imakan.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Preferences (val context: Context) {
+    companion object {
+        const val USER_PREF = "USER_PREF"
+    }
+
+    var sharedPreferences = context.getSharedPreferences(USER_PREF, 0)
+
+    fun setValue(key: String, value: String) {
+        val editor:SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun getValue(key: String) : String? {
+        return sharedPreferences.getString(key, "")
+    }
+}
