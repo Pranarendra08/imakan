5fc9e5cdf97b04b96affc9c7c029d4f1bf8a558f
Pranarendra08 <pranarendra@gmail.com>
2022-02-03 17:53:10 +0700

    update home page

--- app/src/main/java/com/rendra/imakan/home/HomeFragment.kt
+++ app/src/main/java/com/rendra/imakan/home/HomeFragment.kt
@@ -18,4 +18,10 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //preferences.getValue("nama")
+    }
+
 }
