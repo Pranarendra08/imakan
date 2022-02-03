d59536abf1f6be53b71f2ff066c2d9f291a57fc4
Pranarendra08 <pranarendra@gmail.com>
2022-02-03 17:27:18 +0700

    update home page

--- app/src/main/java/com/rendra/imakan/signin/SignInActivity.kt
+++ app/src/main/java/com/rendra/imakan/signin/SignInActivity.kt
@@ -1,14 +1,19 @@
 package com.rendra.imakan.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rendra.imakan.R
+import com.rendra.imakan.home.HomePageActivity
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-
+        btn_login.setOnClickListener {
+            startActivity(Intent(this, HomePageActivity::class.java))
+        }
     }
 }
