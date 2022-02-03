8573cf6842734abb6820b0484ea13cb720a6a1c5
Pranarendra08 <pranarendra@gmail.com>
2022-02-03 16:16:41 +0700

    update sign in page & home page

--- app/src/main/java/com/rendra/imakan/home/HomePageActivity.kt
+++ app/src/main/java/com/rendra/imakan/home/HomePageActivity.kt
@@ -1,14 +1,12 @@
-package com.rendra.imakan.signin
+package com.rendra.imakan.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rendra.imakan.R
 
-class SignInActivity : AppCompatActivity() {
+class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
-
-
+        setContentView(R.layout.activity_home_page)
     }
 }
