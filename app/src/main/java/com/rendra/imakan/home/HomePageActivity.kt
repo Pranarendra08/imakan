aefc7d806cf48febf4ace97adb1d935378ba4524
Pranarendra08 <pranarendra@gmail.com>
2022-02-04 17:17:27 +0700

    update home page

--- app/src/main/java/com/rendra/imakan/home/HomePageActivity.kt
+++ app/src/main/java/com/rendra/imakan/home/HomePageActivity.kt
@@ -2,11 +2,24 @@ package com.rendra.imakan.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.rendra.imakan.R
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        val fragmentHome = HomeFragment()
+
+        setFragment(fragmentHome)
     }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.layout_home, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
