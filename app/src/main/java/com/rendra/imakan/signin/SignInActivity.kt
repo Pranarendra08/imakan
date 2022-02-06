d8d26c3be041213a43ebc236eab8c22fcb139a0d
Pranarendra08 <pranarendra@gmail.com>
2022-02-06 17:55:14 +0700

    update sign up

--- app/src/main/java/com/rendra/imakan/signin/SignInActivity.kt
+++ app/src/main/java/com/rendra/imakan/signin/SignInActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 import com.rendra.imakan.R
 import com.rendra.imakan.home.HomePageActivity
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import com.google.firebase.database.DatabaseReference
+
+import com.google.firebase.database.FirebaseDatabase
+import com.rendra.imakan.signup.SignUpActivity
+
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +19,10 @@ class SignInActivity : AppCompatActivity() {
 
         btn_login.setOnClickListener {
             startActivity(Intent(this, HomePageActivity::class.java))
+        }
+
+        btn_register.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/rendra/imakan/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.rendra.imakan.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rendra.imakan.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
