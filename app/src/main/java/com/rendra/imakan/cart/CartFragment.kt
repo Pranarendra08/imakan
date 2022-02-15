525db11455a97609b43bb1c1dc61928317ae0439
Pranarendra08 <pranarendra@gmail.com>
2022-02-15 14:48:38 +0700

    update order confirmation page

--- app/src/main/java/com/rendra/imakan/cart/CartFragment.kt
+++ app/src/main/java/com/rendra/imakan/cart/CartFragment.kt
@@ -1,11 +1,14 @@
 package com.rendra.imakan.cart
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.rendra.imakan.R
+import com.rendra.imakan.cart.orderConfirmation.OrderConfirmationActivity
+import kotlinx.android.synthetic.main.fragment_cart.*
 
 
 class CartFragment : Fragment() {
@@ -16,6 +19,14 @@ class CartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_beli.setOnClickListener {
+            startActivity(Intent(activity, OrderConfirmationActivity::class.java))
+        }
     }
 
 }
---
+++ app/src/main/java/com/rendra/imakan/cart/payment/PaymentPageActivity.kt
@@ -0,0 +1,12 @@
+package com.rendra.imakan.cart.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rendra.imakan.R
+
+class PaymentPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_page)
+    }
+}
