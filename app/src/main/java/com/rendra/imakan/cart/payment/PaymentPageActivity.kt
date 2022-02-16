4d883ee3636e2cb09f0cee56e17c8451b7059e31
Pranarendra08 <pranarendra@gmail.com>
2022-02-16 16:15:25 +0700

    update payment detail page

--- app/src/main/java/com/rendra/imakan/cart/payment/PaymentPageActivity.kt
+++ app/src/main/java/com/rendra/imakan/cart/payment/PaymentPageActivity.kt
@@ -1,12 +1,19 @@
 package com.rendra.imakan.cart.payment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rendra.imakan.R
+import com.rendra.imakan.cart.paymentDetail.PaymentDetailActivity
+import kotlinx.android.synthetic.main.activity_payment_page.*
 
 class PaymentPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_page)
+
+        btn_bayar.setOnClickListener {
+            startActivity(Intent(this, PaymentDetailActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/rendra/imakan/cart/paymentDetail/PaymentDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.rendra.imakan.cart.paymentDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rendra.imakan.R
+
+class PaymentDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_detail)
+    }
+}
