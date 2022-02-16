d9929de4fae2d21cde59178b8cb6030fde500231
Pranarendra08 <pranarendra@gmail.com>
2022-02-16 16:39:49 +0700

    update payment success page

---
+++ app/src/main/java/com/rendra/imakan/cart/paymentSuccess/PaymentSuccessPageActivity.kt
@@ -0,0 +1,12 @@
+package com.rendra.imakan.cart.paymentSuccess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rendra.imakan.R
+
+class PaymentSuccessPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_success_page)
+    }
+}
