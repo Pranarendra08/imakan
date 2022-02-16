58055e6250d89d47663c821acdd047896401d2a3
Pranarendra08 <pranarendra@gmail.com>
2022-02-16 17:00:33 +0700

    update payment success page

--- app/src/main/java/com/rendra/imakan/cart/paymentDetail/PaymentDetailActivity.kt
+++ app/src/main/java/com/rendra/imakan/cart/paymentDetail/PaymentDetailActivity.kt
@@ -1,12 +1,20 @@
 package com.rendra.imakan.cart.paymentDetail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rendra.imakan.R
+import com.rendra.imakan.cart.paymentSuccess.PaymentSuccessPageActivity
+import com.rendra.imakan.home.HomePageActivity
+import kotlinx.android.synthetic.main.activity_payment_detail.*
 
 class PaymentDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_detail)
+
+        btn_konfirmasi.setOnClickListener {
+            startActivity(Intent(this, PaymentSuccessPageActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/rendra/imakan/cart/paymentSuccess/PaymentSuccessPageActivity.kt
+++ app/src/main/java/com/rendra/imakan/cart/paymentSuccess/PaymentSuccessPageActivity.kt
@@ -1,12 +1,20 @@
 package com.rendra.imakan.cart.paymentSuccess
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rendra.imakan.R
+import com.rendra.imakan.home.HomePageActivity
+import kotlinx.android.synthetic.main.activity_payment_success_page.*
 
 class PaymentSuccessPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_success_page)
+
+        btn_selesai.setOnClickListener {
+            finishAffinity()
+            startActivity(Intent(this, HomePageActivity::class.java))
+        }
     }
 }
