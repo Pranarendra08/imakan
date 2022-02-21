9d48f393ed62fe10c56481098aa30608b49c0f39
Pranarendra08 <pranarendra@gmail.com>
2022-02-21 17:51:38 +0700

    update explore page

--- app/src/main/java/com/rendra/imakan/explore/ExploreFragment.kt
+++ app/src/main/java/com/rendra/imakan/explore/ExploreFragment.kt
@@ -7,19 +7,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.database.*
 import com.rendra.imakan.R
+import com.rendra.imakan.detail.DetailActivity
 import com.rendra.imakan.model.FindExplore
+import com.rendra.imakan.model.ikanDetail
 import com.rendra.imakan.utils.Preferences
 import kotlinx.android.synthetic.main.fragment_explore.*
+import kotlinx.android.synthetic.main.fragment_explore.iv_photo
+import kotlinx.android.synthetic.main.fragment_explore.tv_nama
 
 
 class ExploreFragment : Fragment() {
 
     private lateinit var preferences: Preferences
     private lateinit var mDatabase: DatabaseReference
+    private lateinit var mDatabase2: DatabaseReference
 
     private var dataList = ArrayList<FindExplore>()
+    private var dataList2 = ArrayList<ikanDetail>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +44,18 @@ class ExploreFragment : Fragment() {
         preferences = Preferences(activity!!.applicationContext)
         mDatabase = FirebaseDatabase.getInstance("https://imakan-493ae-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("explore")
 
+        tv_nama.text = preferences.getValue("nama")
+
+        Glide.with(this)
+            .load(preferences.getValue("url"))
+            .apply(RequestOptions.circleCropTransform())
+            .into(iv_photo)
+
+        rv_find_explore.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        rv_explore_image.layoutManager = LinearLayoutManager(context!!.applicationContext)
+
         getData()
+        getData2()
     }
 
     private fun getData() {
@@ -48,7 +68,27 @@ class ExploreFragment : Fragment() {
                 }
 
                 rv_find_explore.adapter = FindExploreAdapter(dataList) {
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, ""+error.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
+    }
+
+    private fun getData2() {
+        mDatabase2.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                dataList2.clear()
+                for (getdataSnapshot in snapshot.children) {
+                    var ikan = getdataSnapshot.getValue(ikanDetail::class.java)
+                    dataList2.add(ikan!!)
+                }
+
+                rv_explore_image.adapter = FindExploreAdapter(dataList2) {
+                    startActivity(Intent(context, DetailActivity::class.java).putExtra("data", it))
                 }
             }
 
--- app/src/main/java/com/rendra/imakan/explore/ExplorePageAdapter.kt
+++ app/src/main/java/com/rendra/imakan/explore/ExplorePageAdapter.kt
@@ -10,10 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.rendra.imakan.R
+import com.rendra.imakan.model.ikanDetail
 import com.rendra.imakan.model.ikanHome
 
-class ExplorePageAdapter(private var data: List<ikanHome>,
-                         private val listener:(ikanHome) -> Unit)
+class ExplorePageAdapter(private var data: List<ikanDetail>,
+                         private val listener:(ikanDetail) -> Unit)
     : RecyclerView.Adapter<ExplorePageAdapter.ViewHolder>() {
 
     lateinit var contextAdapter: Context
@@ -24,7 +25,7 @@ class ExplorePageAdapter(private var data: List<ikanHome>,
     ): ExplorePageAdapter.ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         contextAdapter = parent.context
-        val inflatedView = layoutInflater.inflate(R.layout.row_item_home_pict, parent, false)
+        val inflatedView = layoutInflater.inflate(R.layout.row_item_explore_page, parent, false)
         return ViewHolder(inflatedView)
     }
 
@@ -35,7 +36,22 @@ class ExplorePageAdapter(private var data: List<ikanHome>,
     override fun getItemCount(): Int = data.size
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bindItem(data:ikanHome, listener: (ikanHome) -> Unit, context: Context) {
+        private val tvIkan1:TextView = view.findViewById(R.id.tv_nama_ikan)
+        private val tvIkan2:TextView = view.findViewById(R.id.tv_nama_ikan2)
+        private val tvIkan3:TextView = view.findViewById(R.id.tv_nama_ikan3)
+        private val tvIkan4:TextView = view.findViewById(R.id.tv_nama_ikan4)
+
+        private val tvHarga1:TextView = view.findViewById(R.id.tv_harga)
+        private val tvHarga2:TextView = view.findViewById(R.id.tv_harga2)
+        private val tvHarga3:TextView = view.findViewById(R.id.tv_harga3)
+        private val tvHarga4:TextView = view.findViewById(R.id.tv_harga4)
+
+        private val ivPhoto1:TextView = view.findViewById(R.id.iv_foto_ikan)
+        private val ivPhoto2:TextView = view.findViewById(R.id.iv_foto_ikan2)
+        private val ivPhoto3:TextView = view.findViewById(R.id.iv_foto_ikan3)
+        private val ivPhoto4:TextView = view.findViewById(R.id.iv_foto_ikan4)
+
+        fun bindItem(data:ikanDetail, listener: (ikanDetail) -> Unit, context: Context) {
 
         }
     }
